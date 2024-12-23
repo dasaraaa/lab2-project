95015ee37f91ef5ee411bcd3df89ae1a207a598d
Dasara Rexha <119667338+dasaraaa@users.noreply.github.com>
2024-12-23 15:39:35 +0100

    Initializing project

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter , Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+     <Routes>
+      <Route path="" />
+      <Route path="" />
+      <Route path="" />
+      <Route path="" />
+      <Route path="" />
+     </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
