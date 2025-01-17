d774a82124399858b88c6661e1157950f6854c69
Dasara Rexha <119667338+dasaraaa@users.noreply.github.com>
2025-01-17 14:29:32 +0100

    mongodb-setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './index.css'; // Or the name of your Tailwind CSS file
 import { BrowserRouter , Route, Routes } from "react-router-dom";
 import Footer from './components/Footer';
+import { useEffect } from 'react';
+import axios from "axios";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from './components/Header';
 import Home from './pages/Home';
 import News from './components/News';
+import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <div style={{ fontFamily: 'Lato, sans-serif' }}>
@@ -15,6 +18,8 @@ function App() {
       <Route path="signin" element={<SignIn/>} />
       <Route path="signup" element={<SignUp/>} />
       <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<AboutUs/>} />
+
       <Route path='/news' element ={<News/>} />
      </Routes>
      </BrowserRouter>
